6ea3a5b5d5eda979ec2a7eb3544541cb3ca98d60
Darren Henderson <darrenh@ccsj.edu>
2019-04-06 15:04:50 -0500

    First step toward building a make-reservation component.

--- public/js/load_rooms_drop_down.js
+++ public/js/load_rooms_drop_down.js
@@ -1,19 +1,80 @@
-new Vue({
-  // el: '#room_list',
-  el: '#room_select',
-  data: {
-    selected: '',
-    rooms: []
-  },
+Vue.component('make-reservation-component', {
 
-  mounted() {
-    // Make an ajax request and render the response
-    // alert('alert');
-    // axios.get('/available_rooms').then(response => console.log(response.data));
-    // test
-    axios.get('/available_rooms').then(response => this.rooms = response.data);
+  props: [
+    'customer_no': '',
+    'last_name': '',
+    'first_name': '',
+  ],
 
-    // axios.get('/rooms/get_available').then(response => this.rooms = response.data);
+  data() {
 
+    return {
+      start_date: '',
+      end_date: '',
+
+      selected_room: '',
+      rooms: []
+
+    }
   }
+
+  template: `
+    <label for="full_name">Book room for {{ $customer->last_name }}, {{ $customer->first_name }}</label>
+    <br/>
+
+    <label for="start_date">Start Date</label>
+    <input type="date" name="start_date" id="start_date" v-model="start_date">
+    <br/>
+
+    <label for="end_date">End Date</label>
+    <input type="date" name="end_date" id="end_date" v-model="end_date">
+    <br/>
+
+    <label>Select Room</label>
+    <select v-model="selected_room">
+      <option v-for="room in rooms" v-bind:value="room">@{{ room }}</option>
+    </select>
+    <br />
+    `,
+
+    methods: {
+      onSubmit() {
+        // alert('onSubmit');
+        var data = {
+          'start_date': this.start_date,
+          'end_date': this.end_date,
+          // 'room_category': this.room_category,
+          'room_no': this.selected_room,
+          'amount': 0,
+          // 'customer_no': this.customer_no,
+          // 'created_at': ,
+          // 'updated_at': ,
+        };
+
+        console.log(data);
+
+          axios.post('/customers/registrations', data)
+              .then(function (response) {
+                  console.log(response);
+                })
+                .catch(function (error) {
+                  console.log(error);
+          });
+      }
+    },
+
+    mounted() {
+      // Make an ajax request and render the response
+      // test
+      axios.get('/available_rooms').then(response => this.rooms = response.data);
+
+      // axios.get('/rooms/get_available').then(response => this.rooms = response.data);
+
+    }
+})
+
+new Vue({
+
+  el: '#myForm',
+
 })
--- resources/views/make_reservation.blade.php
+++ resources/views/make_reservation.blade.php
@@ -5,57 +5,12 @@
 
    <!-- <form action="/customers/registrations" method="post"> -->
    <!--  Test trying to feed vales to RoomsController get_available action -->
-   <form action="/customers/{{ $customer->id }}/reservations" method="post">
-     {{--  temporary for testing --}}
-     {{-- <form action="/rooms/test" method="post"> --}}
+   <form action="/customers/registrations" method="post" @submit.prevent="onSubmit">
 
      {{ csrf_field() }}
 
-     <label for="full_name">Book room for {{ $customer->last_name }}, {{ $customer->first_name }}</label><br/>
-
-      <!--  Add start date control -->
-      <label for="start_date">Start Date</label>
-      <input type="date" name="start_date" id="start_date" value=""><br/>
-
-      <!--  Add end date control -->
-      <label for="end_date">End Date</label>
-      <input type="date" name="end_date" id="end_date" value=""><br/>
-
-      <label for="room_category">Select Room Category</label>
-      <select name="room_category" id="room_category">
-         @foreach($room_categories as $room_category)
-           <option value="{{$room_category->category}}">{{$room_category->category}}</option>
-           @endforeach
-      </select><br/>
-
-      <!--  Add room category select control -->
-      <!--  hardcode it? or how do you programmatically populate a select in php/laravel-->
-      <!-- <label for="room_categories">Select Room Category</label>
-      <select name="room_categories">
-          <option value="economy">Economy</option>
-          <option value="deluxe">Deluxe</option>
-          <option value="suite">Suite</option>
-      </select><br/> -->
-
-      <!--  Add room select control -->
-      <!--  need to programmatically populate this control! -->
-      <div id="room_select">
-        <label>Select Room</label>
-
-        <select v-model="selected">
-          <option v-for="room in rooms" v-bind:value="room">
-            @{{ room }}
-          </option>
-        </select>
-        <span>Selected: @{{ selected }}</span>
-
-        {{-- use a hidden input tp pass room number into the request --}}
-        <input type="hidden" name="selected" :value="selected">
-
-        {{-- <ul>
-          <li v-for="room in rooms">@{{ room}}</li>
-        </ul> --}}
-
+      <div id="myForm">
+        <make-reservation-component :customer="{{ $customer }}"></make-reservation-component>
       </div>
 
       <input type="submit" value="Save">
