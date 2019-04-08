1329f83571f5ad18366d63c9f6fb338571f6aa1f
Darren Henderson <darrenh@ccsj.edu>
2019-04-08 11:20:24 -0500

    Made changes to Reservations table seeder so time component was removed for start_date and end_date fields.

--- database/seeds/ReservationsTableSeeder.php
+++ database/seeds/ReservationsTableSeeder.php
@@ -12,15 +12,17 @@ class ReservationsTableSeeder extends Seeder
      */
     public function run()
     {
-      $tz = 'America/Chicago';
+      // $tz = 'America/Chicago';
       $format = 'Y-m-d';
       // $start_date = DateTime::createFromFormat($format, '2009-03-17');
       // Carbon::createFromDate(20119, 3, 17, tz)
 
       DB::table('reservations')->insert([
         'room_no' => 100,
-        'start_date' => DateTime::createFromFormat($format, '2019-03-17'),
-        'end_date' => DateTime::createFromFormat($format, '2019-03-18'),
+        'start_date' => Carbon::create('2019', '03', '17'),
+        'end_date' => Carbon::create('2019', '03', '18'),
+        // 'start_date' => DateTime::createFromFormat($format, '2019-03-17'),
+        // 'end_date' => DateTime::createFromFormat($format, '2019-03-18'),
         'amount' => 0.0,
         'customer_no' => 1,
         'created_at' => DateTime::createFromFormat($format, '2019-03-17'),
@@ -29,8 +31,10 @@ class ReservationsTableSeeder extends Seeder
 
       DB::table('reservations')->insert([
         'room_no' => 101,
-        'start_date' => Carbon::createFromDate(2019, 3, 17, $tz),
-        'end_date' => Carbon::createFromDate(2019, 3, 27, $tz),
+        'start_date' => Carbon::create('2019', '03', '17'),
+        'end_date' => Carbon::create('2019', '03', '27'),
+        // 'start_date' => Carbon::createFromDate(2019, 3, 17, $tz),
+        // 'end_date' => Carbon::createFromDate(2019, 3, 27, $tz),
         'amount' => 0.0,
         'customer_no' => 2,
         'created_at' => DateTime::createFromFormat($format, '2019-03-17'),
